6617ce2db8312010a59d54aeb5800c915dd42ef8
kulli <kulli@MacBook-Pro.local>
2020-03-03 23:11:42 +0530

    initial commit for soundcloud-poc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    width: "40%",
+    height: "175px",
+    url: "https://soundcloud.com/lukkas-2de/maroon-5-memories-lukkas-remix",
+    autoPlay: true,
+    hideRelated: true, // hides details when paused
+    showComments: true,
+    showUser: true,
+    showPlayCount: true,
+    visual: false, // when true hides play count showPlayCount
+    color: "FB5500"
+  };
+
+  render() {
+    const {
+      url,
+      width,
+      height,
+      autoPlay,
+      hideRelated,
+      showComments,
+      showUser,
+      showPlayCount,
+      visual,
+      color
+    } = this.props;
+
+    const src = `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_playcount=${showPlayCount}&amp;visual=${visual}&amp;show_teaser=false`;
+
+    return (
+      <iframe
+        width={width}
+        height={height}
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src={src}
+      />
+    );
+  }
 }
 
 export default App;
